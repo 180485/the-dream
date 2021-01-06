5dbb09ba7645f7453049978a59eec888852f8132
180485 <ismiandhykawaty@icloud.com>
2021-01-06 18:22:19 +0100

    Changging echo text

--- convertor.php
+++ convertor.php
@@ -11,25 +11,26 @@ $cur1 = $_POST['cur1'];
 
 //converter for EURO
 if($cur == "IDR" ){ 
-    echo "<h2>Your Converted Amount is:</h2>";
-    echo "<h3>" .$amount * 17.172,81 . " ". "IDR"."</h3>" ;
+    echo "<h2>Exhange Rate is :</h2>";
+    echo "<h2>"."<font color=blue>" . "Rp" ." " .$amount * 17.172,81 . " "."</h2>" ."</font>" ;
 
 
 }else if ($cur =="AUD" ){
-    echo "<h2>Your Converted Amount is:</h2>";
-    echo "<h3>". $amount * 1.58 ." ". "AUD" ."</h3>" ;
+    echo "<h2>Exhange Rate is:</h2>";
+    echo "<h2>"."<font color=blue>" ."$" ." " . $amount * 1.58 ." ". "(AUD)" ."</h3>" ."</font>";
 
 
 
 }else if($cur=="USD"){
-    echo "<h2>Your Converted Amount is:</h2>";
-    echo "<h3>" . $amount * 1.26 . " ". "USD" ."</h3>" ; ; 
+    echo "<h2>Exhange Rate is:</h2>";
+    echo "<h2>"."<font color=blue>"  . " $ " ." " . $amount * 1.26 . " " ."</h2>"."</font>"; 
 
 }else if($cur=="THB"){
-    echo "<h2>Your Converted Amount is:</h2>";
-    echo "<h3>" . $amount * 36,82 . " ". "THB" ."</h3>" ;;
+    echo "<h2>Exhange Rate is</h2>";
+    echo "<h2>"."<font color=blue>"  . $amount * 36,82 . " ". "THB" ."</h2>" ."</font>";
 
 }
+
 
 
 
