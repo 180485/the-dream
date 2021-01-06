ddb7a236833238573bfa51b79120510ce3c4a027
180485 <ismiandhykawaty@icloud.com>
2021-01-06 19:47:32 +0100

    Making index.html to index.php

--- index.php
+++ index.php
@@ -8,10 +8,47 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Currency Converter</h1>
+<header>
+ <h1>Currency Converter</h1>
+</header>
 
-<div>
-  <form action="convertor.php" method="post">
+<div class ="box">
+<?php 
+
+    if(isset($_POST['submit'])){
+	
+      $amount = $_POST['amount'];
+      $cur = $_POST['cur'];
+      $cur1 = $_POST['cur1'];
+
+    }
+
+//converter for EURO
+
+  if($cur == "IDR" ){ 
+    echo "<h2>Exhange Rate  :</h2>";
+    echo "<h2>" . "Rp" ." " .$amount * 17.172,81 . " "."</h2>";
+
+
+  }else if ($cur =="AUD" ){
+    echo "<h2>Exhange Rate :</h2>";
+    echo "<h2>"."$" ." " . $amount * 1.58 ." ". "(AUD)" ."</h3>" ;
+
+
+
+  }else if($cur=="USD"){
+    echo "<h2>Exhange Rate :</h2>";
+    echo "<h2>". " $ " ." " . $amount * 1.26 . " " ."</h2>"; 
+
+  }else if($cur=="THB"){
+    echo "<h2>Exhange Rate :</h2>";
+    echo "<h2>". $amount * 36,82 . " ". "THB" ."</h2>";
+
+  }
+
+?>
+
+  <form action="" method="post">
 
     <label name="amount">Amount</label>
     <input type="text" id="Camount" name="amount" placeholder="Amount">
@@ -29,8 +66,8 @@
         <option value="THB">THB (Thai baht)</option>
     </select>
   
-    <input type="submit" name="submit" value="Submit">
+    <input type="submit" name="submit" value="Convert">  
   </form>
-</div>
+</div>  
 </body>
 </html>
