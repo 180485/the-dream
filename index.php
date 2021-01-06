a2904677644846480274ba402e23e31516d88a5e
180485 <ismiandhykawaty@icloud.com>
2021-01-06 22:56:14 +0100

    Adding extra country brazil

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
   
     <link href="style.css" rel="stylesheet" />
     <title>Currency Converter</title>
@@ -13,30 +15,36 @@
    <!-- php calculation -->
 
 <?php 
+
     if(isset($_POST['submit'])){
       $amount = $_POST['amount'];
       $cur = $_POST['cur'];
       $cur1 = $_POST['cur1'];
+      
     }
 
 //converter for EURO
   if($cur=="USD"){
-    echo "<h2>"."Exhange Rate : "."$" .$amount * 1.26 . " ". "USD" ." </h2>";
+    echo "<h2>"."Exhange Rate : "."$" . " " .$amount * 1.26 . " "." </h2>";
     echo "<p style='color:green;'>" . "1 EUR = 1.26 USD" ."</p>";
       
 
     }else if ($cur =="AUD" ){
-      echo "<h2>"."Exhange Rate : "."$" .$amount * 1.58 . " ". "AUD" ." </h2>";
+      echo "<h2>"."Exhange Rate : "."$" . " "  .$amount * 1.58 . " " ." </h2>";
       echo "<p style='color:green;'>" . "1 EUR = 1.58 AUD" ."</p>";
        
     }else if($cur == "IDR" ){ 
-      echo "<h2>"."Exhange Rate : " ."Rp" .$amount * 17.172 . " ". "IDR" ." </h2>";
+      echo "<h2>"."Exhange Rate : " ."Rp". " " .$amount * 17.172 . " ". "IDR" ." </h2>";
       echo "<p style='color:green;'>" . "1 EUR =  Rp 17.172 IDR " ."</p>";
        
     }else if($cur=="THB"){
        echo "<h2>"."Exhange Rate : " . $amount * 36,82 . " ". "THB" ." </h2>";
-       echo "<p style='color:green;'>" . "1 EUR = 36.82 THB" ."</p>";
+       echo "<p style='color:green;'>" . "1 EUR = 36.82 Thai Baht" ."</p>";
     
+
+    }else if($cur=="BRL"){
+      echo "<h2>"."Exhange Rate : " . $amount * 6,55 . " ". "BRL" ." </h2>";
+      echo "<p style='color:green;'>" . "1 EUR = 6,55 Brazilian Real " ."</p>";
 
     }
 
@@ -51,8 +59,8 @@
         <option value="EUR">🇪🇺 EUR (Euro)</option>
         <option value="USD"> 🇺🇸 USD (American  Dollar)</option>
         </select>
-
-    <label for="country">Currency</label>
+    
+    <label for="country">Converted to</label>
     <select id="Currency" name="cur">
          <option value="BRL" 🇮> 🇧🇷 BRL (Brazilian Real) </option>
         <option value="USD" selected > 🇺🇸 USD (American  Dollar)</option>
@@ -61,7 +69,7 @@
         <option value="THB"> 🇹🇭 THB (Thai baht)</option>
     </select>
   
-    <input type="submit" name="submit" value="Convert">  
+    <input type="submit" name="submit" value="CONVERT">  
     
   </form>
 </div>  
