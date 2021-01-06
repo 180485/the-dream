31a0882d7786018f5eb0276e2160ad6c230a4ea0
180485 <ismiandhykawaty@icloud.com>
2021-01-06 16:18:38 +0100

    fixing my if statement at php

--- convertor.php
+++ convertor.php
@@ -10,22 +10,22 @@ $cur1 = $_POST['cur1'];
 }
 
 //converter for EURO
-if($cur == "IDR" AND $cur1=="EUR"){ 
+if($cur == "IDR" ){ 
     echo "<h2>Your Converted Amount is:</h2>";
     echo "<h3>" .$amount * 17.172,81 . " ". "IDR"."</h3>" ;
 
 
-}else if ($cur =="AUD" AND $cur1=="EUR"){
+}else if ($cur =="AUD" ){
     echo "<h2>Your Converted Amount is:</h2>";
     echo "<h3>". $amount * 1.58 ." ". "AUD" ."</h3>" ;
 
 
 
-}else if($cur=="USD"AND $cur1=="EUR"){
+}else if($cur=="USD"){
     echo "<h2>Your Converted Amount is:</h2>";
     echo "<h3>" . $amount * 1.26 . " ". "USD" ."</h3>" ; ; 
 
-}else if($cur=="THB" AND $cur1=="EUR"){
+}else if($cur=="THB"){
     echo "<h2>Your Converted Amount is:</h2>";
     echo "<h3>" . $amount * 36,82 . " ". "THB" ."</h3>" ;;
 
